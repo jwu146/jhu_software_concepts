3e151e35e387a1fb0061c49a24738fddb2da0cd4
jwu146 <jwu146@jhu.edu>
2025-06-13 18:43:54 -0500

    setup sphin

---
+++ module_4/source/index.rst
@@ -0,0 +1,20 @@
+.. module_4 documentation master file, created by
+   sphinx-quickstart on Fri Jun 13 18:30:23 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+module_4 documentation
+======================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. automodule:: add
+   :members:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
--- module_4/src/order.py
+++ module_4/src/order.py
@@ -1,16 +1,36 @@
 from .pizza import Pizza
 
 class Order:
-    """Represents a pizza order by a customer."""
+    """
+    Represents a pizza order by a customer.
+
+    :ivar pizzas: List of Pizza objects in the order.
+    :vartype pizzas: list of Pizza
+    :ivar cost: Total cost of the order.
+    :vartype cost: float
+    :ivar paid: Whether the order has been paid.
+    :vartype paid: bool
+    """
 
     def __init__(self):
-        """Initializes an empty order."""
+        """
+        Initializes an empty order.
+
+        Sets up an empty list of pizzas, initializes the cost to zero,
+        and marks the order as unpaid.
+        """
         self.pizzas = []
         self.cost = 0
         self.paid = False
 
     def __str__(self):
-        """Returns a string showing the customer's complete order."""
+        """
+        Returns a string showing the customer's complete order.
+
+        :return: A formatted string representing all pizzas in the order.
+            If the order is empty, indicates no pizzas in the order.
+        :rtype: str
+        """
         if not self.pizzas:
             pizzas_str = "No pizzas in this order."
             return pizzas_str
@@ -24,13 +44,21 @@ class Order:
     def input_pizza(self, *ingredients):
         """
         Adds a pizza to the order, categorizing ingredients automatically.
-        Args:
-            *ingredients: Ingredients for Pizza (order doesn't matter).
+
+        :param ingredients: Ingredients for the Pizza (order doesn't matter).
+        :type ingredients: str
+        :return: None
         """
         pizza = Pizza(*ingredients)
         self.pizzas.append(pizza)
         self.cost += pizza.cost()
 
     def order_paid(self):
-        """Marks the order as paid."""
+        """
+        Marks the order as paid.
+
+        Sets the 'paid' attribute to True.
+
+        :return: None
+        """
         self.paid = True

ee7969493f0126f6117d4fb03302db927298a440
jwu146 <jwu146@jhu.edu>
2025-06-13 08:16:38 -0500

    customer order

---
+++ module_4/src/order.py
@@ -0,0 +1,36 @@
+from .pizza import Pizza
+
+class Order:
+    """Represents a pizza order by a customer."""
+
+    def __init__(self):
+        """Initializes an empty order."""
+        self.pizzas = []
+        self.cost = 0
+        self.paid = False
+
+    def __str__(self):
+        """Returns a string showing the customer's complete order."""
+        if not self.pizzas:
+            pizzas_str = "No pizzas in this order."
+            return pizzas_str
+        else:
+            pizzas_str = "\n".join(str(pizza) for pizza in self.pizzas)
+            return (
+                f"Customer Requested:\n"
+                f"{pizzas_str}\n"
+            )
+
+    def input_pizza(self, *ingredients):
+        """
+        Adds a pizza to the order, categorizing ingredients automatically.
+        Args:
+            *ingredients: Ingredients for Pizza (order doesn't matter).
+        """
+        pizza = Pizza(*ingredients)
+        self.pizzas.append(pizza)
+        self.cost += pizza.cost()
+
+    def order_paid(self):
+        """Marks the order as paid."""
+        self.paid = True
